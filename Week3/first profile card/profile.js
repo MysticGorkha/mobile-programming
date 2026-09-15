775e249d65405b0aaa66516545a709ea33e2118b
Sijan Khadka <sijankhad@gmail.com>
2026-09-15 08:31:25 +0545

    profile card

---
+++ Week3/first profile card/profile.js
@@ -0,0 +1,12 @@
+$(document).ready(function() 
+            {
+                    $("#qr","hide-qr").hide();
+                $("#show-qr").click(function() {
+                    $("#qr", "#hide-qr").show();
+                });
+                $("#hide-qr").click(function() {
+                    $("#qr", "#hide-qr").hide();
+                });
+
+                $(".card").hover(function() {
+            });
